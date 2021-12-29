5af3d923e185ab450729850bcf8f855f65725863
graphieros <alec.lloyd.probert@gmail.com>
2021-12-29 16:01:32 +0100

    adds content to the Projects drawer

--- src/components/drawers/Projects.vue
+++ src/components/drawers/Projects.vue
@@ -39,7 +39,386 @@ const language = computed(() => {
     <h1 class="big-title title-font font-shade white mt3">
       {{ textContent.title[language] }}
     </h1>
+
+    <w-flex class="mt5 white">
+      <w-card class="projects-card pa5 mb5">
+        <w-flex wrap>
+          <w-flex class="xs12 mb5">
+            <h1 class="title-font bold font-shade">AgilePkr</h1>
+          </w-flex>
+          <w-flex class="xs12 md8 align-center justify-center">
+            <w-flex wrap>
+              <w-flex class="xs12">
+                <h4 align="left" class="title-font pr5">
+                  {{ textContent.projects.agilePkr.intro[language] }}
+                </h4>
+              </w-flex>
+              <w-flex class="xs12 mt5">
+                <h4 align="left" class="title-font pr5">
+                  {{ textContent.projects.agilePkr.stack[language] }}
+                </h4>
+              </w-flex>
+            </w-flex>
+          </w-flex>
+
+          <w-flex wrap class="py5">
+            <w-flex class="xs12 mb5">
+              <img
+                class="fw box-shadow"
+                src="../../assets/screenshots/agilepkr_table.png"
+              /><br />
+            </w-flex>
+            <w-flex class="xs12">
+              <img
+                class="fw box-shadow"
+                src="../../assets/screenshots/agilepkr_stats_2.png"
+              />
+            </w-flex>
+          </w-flex>
+
+          <w-flex class="xs12">
+            <w-tooltip bg-color="blue-dark2" color="white">
+              <template #activator="{ on }">
+                <img
+                  v-on="on"
+                  class="mx2 drop-shadow tech-image"
+                  src="../../assets/logos/ts.png"
+                  alt="TypeScript"
+                />
+              </template>
+              <h4 class="title-font">TypeScript</h4>
+            </w-tooltip>
+
+            <w-tooltip bg-color="vue--bg" color="black">
+              <template #activator="{ on }">
+                <img
+                  v-on="on"
+                  class="mx2 drop-shadow tech-image"
+                  src="../../assets/logos/vue2.png"
+                  alt="Vue2"
+                />
+              </template>
+              <h4 class="title-font">Vue2 + VueX</h4>
+            </w-tooltip>
+            <w-tooltip bg-color="blue-light3" color="black">
+              <template #activator="{ on }">
+                <img
+                  v-on="on"
+                  class="mx2 drop-shadow tech-image"
+                  src="../../assets/logos/vuetify.png"
+                  alt="Vuetify"
+                />
+              </template>
+              <h4 class="title-font">Vuetify</h4>
+            </w-tooltip>
+            <w-tooltip bg-color="orange-light1" color="black">
+              <template #activator="{ on }">
+                <img
+                  v-on="on"
+                  class="mx2 drop-shadow tech-image"
+                  src="../../assets/logos/firebase.png"
+                  alt="Firebase"
+                />
+              </template>
+              <h4 class="title-font">Firebase</h4>
+            </w-tooltip>
+          </w-flex>
+
+          <w-flex class="align-end justify-end">
+            <a href="https://agilepkr.graphieros.com" target="_blank">
+              <Button
+                class="title-font"
+                xl
+                round
+                shadow
+                outline
+                color="white"
+                ico="mdi mdi-eye-plus"
+                :content="textContent.projects.agilePkr.cta[language]"
+                :action="() => null"
+              />
+            </a>
+          </w-flex>
+        </w-flex>
+      </w-card>
+    </w-flex>
+
+    <w-flex class="mt5 white">
+      <w-card class="projects-card pa5 mb5">
+        <w-flex wrap>
+          <w-flex class="xs12 mb5">
+            <h1 class="title-font bold font-shade">Graphieros (React)</h1>
+          </w-flex>
+        </w-flex>
+
+        <w-flex class="xs12 md8 align-center justify-center">
+          <w-flex wrap>
+            <w-flex class="xs12">
+              <h4 align="left" class="title-font pr5">
+                {{ textContent.projects.graphierosReact.intro[language] }}
+              </h4>
+            </w-flex>
+            <w-flex class="xs12 mt5">
+              <h4 align="left" class="title-font pr5">
+                {{ textContent.projects.graphierosReact.stack[language] }}
+              </h4>
+            </w-flex>
+          </w-flex>
+        </w-flex>
+
+        <w-flex wrap class="py5">
+          <w-flex class="xs12 mb5">
+            <img
+              class="fw box-shadow"
+              src="../../assets/screenshots/graphieros_react_home1.png"
+            /><br />
+          </w-flex>
+          <w-flex class="xs12 mb5">
+            <img
+              class="fw box-shadow"
+              src="../../assets/screenshots/graphieros_react_words.png"
+            />
+          </w-flex>
+          <w-flex class="xs12">
+            <img
+              class="fw box-shadow"
+              src="../../assets/screenshots/graphieros_react_space.png"
+            />
+          </w-flex>
+        </w-flex>
+
+        <w-flex class="xs12">
+          <w-tooltip bg-color="yellow" color="black">
+            <template #activator="{ on }">
+              <img
+                v-on="on"
+                class="mx2 drop-shadow tech-image"
+                src="../../assets/logos/js.png"
+                alt="JavaScript"
+              />
+            </template>
+            <h4 class="title-font">JavaScript (ES6+)</h4>
+          </w-tooltip>
+          <w-tooltip bg-color="blue-light3" color="black">
+            <template #activator="{ on }">
+              <img
+                v-on="on"
+                class="mx2 drop-shadow tech-image"
+                src="../../assets/logos/react.png"
+                alt="ReactJs"
+              />
+            </template>
+            <h4 class="title-font">ReactJs</h4>
+          </w-tooltip>
+        </w-flex>
+
+        <w-flex class="align-end justify-end">
+          <a href="https://graphieros.com" target="_blank">
+            <Button
+              class="title-font"
+              xl
+              round
+              shadow
+              outline
+              color="white"
+              ico="mdi mdi-eye-plus"
+              :content="textContent.projects.graphierosReact.cta[language]"
+              :action="() => null"
+            />
+          </a>
+        </w-flex>
+      </w-card>
+    </w-flex>
+
+    <w-flex class="mt5 white">
+      <w-card class="projects-card pa5 mb5">
+        <w-flex wrap>
+          <w-flex class="xs12 mb5">
+            <h1 class="title-font bold font-shade">Graphieros (Vue3)</h1>
+          </w-flex>
+        </w-flex>
+
+        <w-flex class="xs12 md8 align-center justify-center">
+          <w-flex wrap>
+            <w-flex class="xs12">
+              <h4 align="left" class="title-font pr5">
+                {{ textContent.projects.graphierosVue.intro[language] }}
+              </h4>
+            </w-flex>
+            <w-flex class="xs12 mt5">
+              <h4 align="left" class="title-font pr5">
+                {{ textContent.projects.graphierosVue.stack[language] }}
+              </h4>
+            </w-flex>
+          </w-flex>
+        </w-flex>
+
+        <w-flex wrap class="py5">
+          <w-flex class="xs12 mb5">
+            <img
+              class="fw box-shadow"
+              src="../../assets/screenshots/graphieros_vue_home1.png"
+            /><br />
+          </w-flex>
+          <w-flex class="xs12 mb5">
+            <img
+              class="fw box-shadow"
+              src="../../assets/screenshots/graphieros_vue_editor.png"
+            />
+          </w-flex>
+          <w-flex class="xs12">
+            <img
+              class="fw box-shadow"
+              src="../../assets/screenshots/graphieros_vue_expl.png"
+            />
+          </w-flex>
+        </w-flex>
+
+        <w-flex class="xs12">
+          <w-tooltip bg-color="yellow" color="black">
+            <template #activator="{ on }">
+              <img
+                v-on="on"
+                class="mx2 drop-shadow tech-image"
+                src="../../assets/logos/js.png"
+                alt="JavaScript"
+              />
+            </template>
+            <h4 class="title-font">JavaScript (ES6+)</h4>
+          </w-tooltip>
+
+          <w-tooltip bg-color="vue--bg" color="black">
+            <template #activator="{ on }">
+              <img
+                v-on="on"
+                class="mx2 drop-shadow tech-image"
+                src="../../assets/logos/vue3.png"
+                alt="Vue3"
+              />
+            </template>
+            <h4 class="title-font">Vue3</h4>
+          </w-tooltip>
+        </w-flex>
+
+        <w-flex class="align-end justify-end">
+          <a href="https://en.graphieros.com" target="_blank">
+            <Button
+              class="title-font"
+              xl
+              round
+              shadow
+              outline
+              color="white"
+              ico="mdi mdi-eye-plus"
+              :content="textContent.projects.graphierosVue.cta[language]"
+              :action="() => null"
+            />
+          </a>
+        </w-flex>
+      </w-card>
+    </w-flex>
+
+    <w-flex class="mt5 white">
+      <w-card class="projects-card pa5 mb5">
+        <w-flex wrap>
+          <w-flex class="xs12 mb5">
+            <h1 class="title-font bold font-shade">Toilet Mogul</h1>
+          </w-flex>
+        </w-flex>
+
+        <w-flex class="xs12 md8 align-center justify-center">
+          <w-flex wrap>
+            <w-flex class="xs12">
+              <h4 align="left" class="title-font pr5">
+                {{ textContent.projects.toiletMogul.intro[language] }}
+              </h4>
+            </w-flex>
+            <w-flex class="xs12 mt5">
+              <h4 align="left" class="title-font pr5">
+                {{ textContent.projects.toiletMogul.stack[language] }}
+              </h4>
+            </w-flex>
+          </w-flex>
+        </w-flex>
+
+        <w-flex wrap class="py5">
+          <w-flex class="xs12">
+            <img
+              class="fw box-shadow"
+              src="../../assets/screenshots/toiletMogul_home1.png"
+            /><br />
+          </w-flex>
+        </w-flex>
+
+        <w-flex class="xs12">
+          <w-tooltip bg-color="blue-dark2" color="white">
+            <template #activator="{ on }">
+              <img
+                v-on="on"
+                class="mx2 drop-shadow tech-image"
+                src="../../assets/logos/ts.png"
+                alt="TypeScript"
+              />
+            </template>
+            <h4 class="title-font">TypeScript</h4>
+          </w-tooltip>
+
+          <w-tooltip bg-color="vue--bg" color="black">
+            <template #activator="{ on }">
+              <img
+                v-on="on"
+                class="mx2 drop-shadow tech-image"
+                src="../../assets/logos/vue3.png"
+                alt="Vue3"
+              />
+            </template>
+            <h4 class="title-font">Vue3 + VueX + composition API</h4>
+          </w-tooltip>
+
+          <w-tooltip bg-color="blue-light3" color="black">
+            <template #activator="{ on }">
+              <img
+                v-on="on"
+                class="mx2 drop-shadow tech-image"
+                src="../../assets/logos/waveui.png"
+                alt="Vue3"
+              />
+            </template>
+            <h4 class="title-font">waveUI</h4>
+          </w-tooltip>
+        </w-flex>
+
+        <w-flex class="align-end justify-end">
+          <a href="https://toilet-mogul.graphieros.com" target="_blank">
+            <Button
+              class="title-font"
+              xl
+              round
+              shadow
+              outline
+              color="white"
+              ico="mdi mdi-eye-plus"
+              :content="textContent.projects.toiletMogul.cta[language]"
+              :action="() => null"
+            />
+          </a>
+        </w-flex>
+      </w-card>
+    </w-flex>
   </w-card>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.projects-card {
+  width: 100%;
+  background: rgba(255, 255, 255, 0.205);
+}
+
+.tech-image {
+  width: 50px;
+}
+
+.agilepkr-logo {
+  height: 50px;
+}
+</style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -58,6 +58,64 @@ export const store = createStore<State>({
           fr: "Projets",
           en: "Projects",
         },
+        projects: {
+          agilePkr: {
+            intro: {
+              fr: "Solution ludique d'agile poker pour faire des sessions d'estimations de user-stories un moment agréable et productif, tout spécialement pour les équipes agiles en télétravail.",
+              en: "Original implementation of the classic 'agile poker' to make estimating sessions of user stories a playful and productive gathering, especially for remote agile teams.",
+            },
+            stack: {
+              fr: "PWA codée en Vue2 + Vuetify + Firebase",
+              en: "PWA written in Vue2 + Vuetify + Firebase",
+            },
+            cta: {
+              fr: "Accéder à l'application",
+              en: "Check out the app",
+            },
+          },
+          graphierosReact: {
+            intro: {
+              fr: "Présentation de la langue construite 'graphieros': origines, fonctionnement, lexique, éditeur de glyphes en SVG et autres ressources.",
+              en: "Presentation of the 'graphieros' conlang: origins, how the languag works, glyphs SVG editor and other resources.",
+            },
+            stack: {
+              fr: "Application codée en React avec vanilla JS",
+              en: "Application written in React with Vanilla JS",
+            },
+            cta: {
+              fr: "Accéder à l'application",
+              en: "Check out the app",
+            },
+          },
+          graphierosVue: {
+            intro: {
+              fr: "Présentation de la langue construite graphieros, dans une version revue en bilingue, et un éditeur de texte SVG amélioré.",
+              en: "Presentation ",
+            },
+            stack: {
+              fr: "Application codée avec Vue3 et Vanilla JS",
+              en: "Application written in Vue3 with Vanilla JS",
+            },
+            cta: {
+              fr: "Accéder à l'application",
+              en: "Check out the app",
+            },
+          },
+          toiletMogul: {
+            intro: {
+              fr: "Un jeu de trading 'idle' amusant et ironique que le joueur peut choisir de laisser tourner ou essayer frénétiquement de gagner autant d'argent que possible. Achetez et vendez des actions de l'Universal Toilet Paper Index, altérez le marché, évitez la SEC, achetez et vendez des biens immobiliers, créez une entreprise et amenez-la sur la lune.",
+              en: "A funny and ironic trading idle game the player can choose to leave to play on itself, or frenetically try to earn as much cash as possible. Buy and sell shares of the Universal Toilet Paper Index, tamper with the market, avoid the SEC, buy and sell real estate, create a company and bring it to the moon.",
+            },
+            stack: {
+              fr: "Application codée avec Vue3, TypeScript et WaveUI",
+              en: "Application written in Vue3, TypeScript and WaveUI",
+            },
+            cta: {
+              fr: "Accéder au jeu",
+              en: "Check out the game",
+            },
+          },
+        },
       },
       drawerStack: {
         title: {
