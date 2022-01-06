ff48aa21516eecacd28f12540922dbfcc383a88c
graphieros <alec.lloyd.probert@gmail.com>
2022-01-06 07:51:38 +0100

    removing tests

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  transform: {
-    "^.+\\.vue$": "vue3-jest",
-  },
-  moduleFileExtensions: ["json", "js", "jsx", "ts", "tsx", "vue"],
-};
